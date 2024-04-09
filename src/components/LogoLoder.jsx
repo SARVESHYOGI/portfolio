cb885248fcbef3d38c51f427644f198e86700c04
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-04-09 18:29:42 +0530

    completed components section

---
+++ src/components/LogoLoder.jsx
@@ -0,0 +1,13 @@
+const LogoLoader = () => {
+    return (
+        <div className="w-full h-full min-h-screen flex justify-center items-center bg-[#020c1b]">
+            <img
+                className="w-48  animate-custom mix-blend-lighten"
+                src="https://res.cloudinary.com/dljzvv5dh/image/upload/v1709129503/Portfolio/ydrhy6ko8qbiclqxykfi.png"
+                alt="logo"
+            />
+        </div>
+    );
+};
+
+export default LogoLoader;

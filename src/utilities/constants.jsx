215a44d7ccc5e2793204bec55cf376b485615e3e
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-07-22 23:22:10 +0530

    updated projects

--- src/utilities/constants.jsx
+++ src/utilities/constants.jsx
@@ -58,7 +58,7 @@ export const social_links = [
     },
 
     {
-        url: "https://leetcode.com/sarveshyogi2005/",
+        url: "https://leetcode.com/sarvesh2005/",
         icon: <SiLeetcode className="w-[22px] h-[22px]" />,
         name: "Leetcode",
     },
@@ -66,140 +66,151 @@ export const social_links = [
 
 export const mainProjects = [
     {
-        title: "FlixAI",
+        title: "E-BHARAT",
         description:
-            "FlixAI, replicates an OTT platform with integrated ChatGPT for personalized movie recommendations. Developed with React, Firebase, and Redux, it ensures real-time updates, seamless authentication, and a mobile-friendly design.",
-        tags: ["React.js", "Tailwind CSS", "Firebase", "Redux", "OpenAI"],
-        github: "https://github.com/yash2324/FlixAI",
-        demo: "https://flix-ai.vercel.app/",
-        image:
-            "https://res.cloudinary.com/dljzvv5dh/image/upload/v1708365673/Portfolio/ijn4dxogasi8y2errjpn.png",
+            "created E-bharat, a fully functional shopping e-commerce website using React.js, Tailwind CSS, Firebase, and Redux, where admins can add products and customers can make purchases.",
+        tags: ["React.js", "Tailwind CSS", "Firebase", "Redux"],
+        github: "https://github.com/SARVESHYOGI/Ecom",
+        demo: "https://ecom-orpin-iota.vercel.app/",
+        image: 'https://res.cloudinary.com/dztzgqzjp/image/upload/v1721666327/Screenshot_2024-07-22_174232_bnrxqc.png',
     },
     {
-        title: "Swift Flavours",
+        title: "IETE-TSEC",
         description:
-            "I made a food ordering app's UI fetching realtime restaurant data from Swiggy's API. Also used Redux to build the cart functionality and make the site more efficient. Also, made the website very mobile friendly",
-        tags: ["React.js", "Tailwind CSS", "Redux", "Shimmer UI", "Jest"],
-        github: "https://github.com/yash2324/Swift-Flavours",
-        demo: "https://swift-flavours.vercel.app/",
+            "Built the official website for IETE-TSEC , Technical Society of my college.Leading a team and emphasis on Git Workflow, I developed the site using React, Tailwind and Framer-motion.",
+        tags: ["React.js", "Tailwind CSS", "Express", "Material UI", "Framer-motion"],
+        github: "https://github.com/SARVESHYOGI",
+        demo: "https://www.ietetsec.in/",
         image:
-            "https://res.cloudinary.com/dljzvv5dh/image/upload/v1707853866/Portfolio/Swiftflavours-screenshots/sip56zuqbp8pmyg4ica7.png",
+            "https://res.cloudinary.com/dztzgqzjp/image/upload/v1721666195/Screenshot_2024-07-22_175341_uqewzt.png",
     },
     {
-        title: "CSI Innowave",
+        title: "realtime-tracker",
         description:
-            "Built the official website for CSI Innowave , Technical Society of my college.Leading a team and emphasis on Git Workflow, I developed the site using React, Tailwind and backend using Node , Express and Mongo DB.Also, used Cloudinary for image hosting.",
-        tags: ["React.js", "Tailwind CSS", "Express", "Acertinity UI", "Node"],
-        github: "https://github.com/yash2324/CSI-Innowave",
-        demo: "https://csiinnowave.com",
-        image:
-            "https://res.cloudinary.com/dljzvv5dh/image/upload/v1709126018/Portfolio/zvc0uoeobifdbknwov7p.png",
+            "A real-time tracking application developed with Node.js, Socket.io, and Express. This project enables live updates and tracking of data, providing an interactive user experience. It demonstrates real-time communication and data synchronization using WebSocket technology.",
+        tags: ["node.js", "socket.io", "JavaScript", "Express"],
+        github: "https://github.com/SARVESHYOGI/realtime-tracker",
+        demo: "https://realtime-tracker-zg12.onrender.com/",
+        image: "https://res.cloudinary.com/dztzgqzjp/image/upload/v1721670630/Screenshot_2024-07-22_232002_kfl0li.png"
     },
+
+
     {
-        title: "Meme Generator",
+        title: "TODOS",
         description:
-            "Made the front-end for a Crime Analysis System , using the past crimes data of India the model predicts the crime stats of a particular district for the upcoming years and also visualises the past stats of that district and area",
-        tags: ["React.js", "HTML", "CSS", "JavaScript"],
-        github: "https://github.com/yash2324/Meme-Generator-2",
-        demo: "https://meme-generator-yg.netlify.app/",
+            " Created a fully functional to-do application using React, Redux, and Tailwind CSS. The app features intuitive task management, real-time updates, and a sleek user interface. It demonstrates my ability to build efficient and user-friendly applications.",
+        tags: ["React.js", "Redux", "HTML", "Tailwind-CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/TodoinReact",
+        demo: "https://todoisnreact-nu.vercel.app/",
         image:
-            "https://res.cloudinary.com/dljzvv5dh/image/upload/v1708366940/Portfolio/Swiftflavours-screenshots/dron8hyjrqtbpjtdj22g.png",
+            "https://res.cloudinary.com/dztzgqzjp/image/upload/v1721666312/Screenshot_2024-07-22_175248_kbqbhr.png",
     },
 ];
 
 export const otherProjects = [
     {
-        title: "Smart Secure",
+        title: "Tinder-Cards",
         description:
-            "Made the front-end for a Crime Analysis System , using the past crimes data of India the model predicts the crime stats of a particular district for the upcoming years and also visualises the past stats of that district and area",
-        tags: ["React.js", "Tailwind CSS", "Express", "Acertinity UI", "Node"],
-        github: "https://github.com/yash2324/CSI-Innowave",
-        demo: "https://csiinnowave.vercel.app/",
+            "created a Tinder-like card application with sleek design and smooth interactions, using React, the Tinder npm package, MongoDB, Express, and Material UI.",
+        tags: ["React.js", "Tailwind CSS", "mongoDB", "Express", "Node"],
+        github: "https://github.com/SARVESHYOGI/tinder-clone",
+        demo: "https://tinder-clone-chi.vercel.app/",
+        // image:
+        //     "https://res.cloudinary.com/dztzgqzjp/image/upload/v1721666328/Screenshot_2024-07-22_174543_gulbij.png",
     },
     {
-        title: "Tenzies",
+        title: "Login-Logout-API",
         description:
-            "I made a basic tenzies Game , using React made the game more fun by adding confetti, Also used hooks to manage the state of the game",
-        tags: ["React.js", "JavaScript", "HTML", "CSS"],
-        github: "https://github.com/yash2324/Tenzies",
-        demo: "https://tenzies-game-yg.netlify.app/",
+            "A secure login and logout API built with React.js, Express, MongoDB, and Node.js. This application utilizes JWT for authentication and Bcrypt for password hashing, ensuring robust security measures. The demo showcases a streamlined user authentication process with a clean, responsive interface.",
+        tags: ["React.js", "Express", "MongoDB", "Node", "JWT", "Bcrypt"],
+        github: "https://github.com/SARVESHYOGI/login_logout_app",
+        demo: "https://login-logout-app.vercel.app/login",
+    },
+
+    {
+        title: "Add your Blog",
+        description:
+            "A blog application built with React.js, Node.js, Express, and MongoDB. This platform allows users to create, read, update, and delete blog posts. It features a user-friendly interface for managing content and demonstrates full-stack development capabilities with seamless integration between the frontend and backend.",
+        tags: ["Reactjs", "Node.js", "Express", "MongoDB"],
+        github: "https://github.com/SARVESHYOGI/blog",
+        demo: "https://blog-88o5l6n6k-sarvesh-yogis-projects.vercel.app/",
     },
     {
-        title: "Chat SAT",
+        title: "Git-Profile-Checker",
         description:
-            "A basic frontend of a SAT prep Application built using Next.js,Typescript,Tailwind CSS.",
-        tags: ["Next.js", "Tailwind CSS", "TypeScript"],
-        github: "https://github.com/yash2324/Chat-SAT",
-        demo: "https://chat-sat-yg.vercel.app/",
-    },
-    {
-        title: "Save Tab Chrome Extension",
-        description:
-            "An extension to save all the links you want to bookmark but in an ordered manner.The link attached is of how the extension would look to actually try it kindly download and deploy the project.",
-        tags: ["HTML", "CSS", "JavaScript", "Local Storage"],
-        github: "https://github.com/yash2324/save-tab-chrome-extension",
-        demo: "https://leadstracker-chrome-extension.netlify.app/",
-    },
-    {
-        title: "AirBnb Landing Page",
-        description:
-            "A basic Landing page to display services offered at an AirBnb built while learning React from Scrimba",
-        tags: ["React", "HTML", "CSS", "JavaScript"],
-        github: "https://github.com/yash2324/Airbnb-clone-scrimba",
-        demo: "https://airbnb-clone-yg.netlify.app/",
-    },
-    {
-        title: "React Info",
-        description:
-            "A static Page listing all the information about React and its features.My first project to explore React.",
-        tags: ["React", "HTML", "Vanilla CSS", "JavaScript"],
-        github: "https://github.com/yash2324/React-proj-1",
-        demo: "https://react-proj1-yg.netlify.app/",
-    },
-    {
-        title: "Notes App",
-        description:
-            "This is a basic notes app to locally make notes , I have made some changes to the already given codes on Scrimba",
-        tags: ["HTML", "CSS", "JavaScript", "React"],
-        github: "https://github.com/yash2324/Notes-App",
-        demo: "https://notes-app-yg.netlify.app/",
-    },
-    {
-        title: "BlackJack Game",
-        description:
-            "Welcome to the Blackjack Game repository! This simple and interactive website is designed to let users enjoy the classic card game of Blackjack right in their web browsers. Whether you're a seasoned pro or new to the world of card games, this project provides a fun way to test your luck and strategy.",
-        tags: ["HTML5", "CSS", "JavaScript"],
-        github: "https://github.com/yash2324/BlackJackJS",
-        demo: "https://blackjack-game-yg.netlify.app/",
-    },
-    {
-        title: "Counter",
-        description:
-            "Welcome to the Subway Passenger Counter, a simple JavaScript-based website that helps you keep track of the number of passengers entering a subway station. This project provides a user-friendly interface for station attendants or operators to monitor and update passenger counts in real-time.",
-        tags: ["HTML5", "CSS", "JavaScript", "DOM"],
-        github: "https://github.com/yash2324/Counter",
-        demo: "https://gleaming-tanuki-a47424.netlify.app/",
+            "A GitHub profile checker built with React.js, Redux, and Material UI. It lets users search and view profiles using the GitHub API, with a responsive and user-friendly interface.",
+        tags: ["React.js", "redux", "Material UI", "api"],
+        github: "https://github.com/SARVESHYOGI/git-c",
+        demo: "https://git-c.vercel.app/",
     },
     {
         title: "Tic Tac Toe Game",
-        description: "A basic Tic Tac Toe Game",
-        tags: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/yash2324",
-        demo: "https://tic-tac-toe-yg.netlify.app/",
+        description: "A basic Tic Tac Toe game created with React, CSS, and JavaScript. It features a simple, interactive interface for a classic game experience.",
+        tags: ["React", "CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/tic-tac-toe",
+        demo: "https://tic-tac-toe-five-ruby.vercel.app/",
     },
+    {
+        title: "Password-Generator",
+        description:
+            "A password generator built with React, HTML, CSS, and JavaScript. It allows users to create strong, customizable passwords with various complexity options.",
+        tags: ["React", "HTML", "CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/password",
+        demo: "https://password-s274.vercel.app/",
+    },
+    {
+        title: "Stopwatch-Timer",
+        description:
+            "A stopwatch and timer application developed with React, HTML, Vanilla CSS, and JavaScript. It features a clean interface for accurate timekeeping and simple functionality.",
+        tags: ["React", "HTML", "Vanilla CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/Stopwatch-Timer",
+        demo: "https://sarveshyogi.github.io/Stopwatch-Timer/",
+    },
+    {
+        title: "backgroundchanger",
+        description:
+            " A background changer application built with React and JavaScript. It allows users to locally change backgrounds, with enhancements made to existing Scrimba code.",
+        tags: ["React-Hooks", "JavaScript", "React"],
+        github: "https://github.com/SARVESHYOGI/backgroundchanger",
+        demo: "https://backgroundchanger-eta.vercel.app/",
+    },
+    {
+        title: "Checkvowels",
+        description:
+            " A simple application for checking vowels in a given text, built with HTML5, CSS, and JavaScript. It offers a straightforward interface for vowel identification and counting.",
+        tags: ["HTML5", "CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/Checkvowels",
+        demo: "https://sarveshyogi.github.io/Checkvowels/",
+    },
+    {
+        title: "dark-light-mode",
+        description:
+            " A dark and light mode toggle feature built with React, Redux, JavaScript, and Tailwind CSS. It allows users to switch between themes for a customizable user experience.",
+        tags: ["React", "Redux", "JavaScript", "Taliwind CSS"],
+        github: "https://github.com/SARVESHYOGI/dark-light-mode",
+        demo: "https://dark-light-mode-fawn.vercel.app/",
+    },
+
     {
         title: "Parallax Animation",
         description:
-            "A Landing page with Parallax Animation , made using HTML and CSS",
+            "A parallax animation project created with HTML5 and CSS. It features dynamic, layered scrolling effects for a visually engaging experience.",
         tags: ["HTML5", "CSS"],
-        github: "https://github.com/yash2324/",
-        demo: "https://parallax-animation-yg.netlify.app/",
+        github: "https://github.com/SARVESHYOGI/parallex",
+        demo: "https://parallex-fawn.vercel.app/",
     },
     {
-        title: "Coffee Card",
-        description: "A Coffee Card made using HTML and CSS",
-        tags: ["HTML5", "CSS"],
-        github: "https://github.com/yash2324/",
-        demo: "https://coffee-card-yg.netlify.app/",
+        title: "currency-converter",
+        description: "A currency converter application built with React, Tailwind CSS, and JavaScript. It uses APIs to provide real-time exchange rates and convert currencies seamlessly.",
+        tags: ["React", "Api", "Tailwind CSS", "JavaScript"],
+        github: "https://github.com/SARVESHYOGI/currency-converter",
+        demo: "https://currency-converter-swart-nu.vercel.app/",
+    },
+    {
+        title: "game-sps-",
+        description: "A game of Rock, Paper, Scissors (SPS) developed with JavaScript, HTML, and CSS. It features interactive gameplay with a user-friendly interface.",
+        tags: ["javascript", "HTML", "CSS"],
+        github: "https://github.com/SARVESHYOGI/game-sps-",
+        demo: "https://sarveshyogi.github.io/game-sps-/",
     },
 ];
